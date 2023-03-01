94fd0520f535550f0bade2b99de43c1551517e57
Enemirovskaya <elenanemirovskaya1984@gmail.com>
2023-02-28 20:24:40 -0500

    client and server listen to each other

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import {useState} from "react";
 
 function App() {
+  const[message, setMessage] = useState("")
+  const where = async()=>{
+  const response = await fetch("/where")
+  const data = await response.text();
+  setMessage(data);
+}
   return (
     <>
     <h1>GitPayd</h1>
     <h2>Comming Soon!</h2>
+    <button on onClick={where}>Where is the server?</button>
+    <p>Server responded: {message}</p>
     </>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const path = require("path");
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3001
-const BUILD_PATH = path.resolve("../client/build")
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const BUILD_PATH = path.resolve("../client/build");
 
 app.use(express.static(BUILD_PATH));
 
 
 
-app.get("/ping", (req, res)=> {res.send("pong!");
+app.get("/where", (req, res)=> {res.send("here!");
 });
 
 app.listen(PORT, () => {
