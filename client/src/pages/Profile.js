ac107e0a9dc6639d3588ac63a517727bca8c6a70
apisdriven <apisdriven@gmail.com>
2023-03-06 20:18:15 -0500

    Added card to profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,13 +1,24 @@
+import userEvent from "@testing-library/user-event";
+import { useAuth } from "utils/auth.js";
+
 export default function Profile(){
+    const {user}=useAuth();
     return(
     <>
         <section>
             <header>
                 <h2>Profile</h2>
             </header>
-            <blockquote>
-               
-            </blockquote>
+        <div class="card">
+            <div class="container">
+            <h4><b>User ID: {user.userId}</b></h4>
+            <p>
+                Username: {user.username}<br></br>
+                Email: {user.email}<br></br>
+                Signature: {user.userId}
+            </p>
+            </div>
+        </div>
         </section>
     </>
     )
