0bf0f27853909526a2df66a51862b18c2a5b551e
Janet Sanchez <janetmarizolsanchez@gmail.com>
2023-03-07 00:28:16 -0700

    Auth changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,8 @@
 // import Receipt from "components/Receipt.js";
 import { useQuery, useMutation } from "@apollo/react-hooks";
-import { ME } from "../utils/queries";
+
 
 const Home = () => {
-  const { loading, data } = useQuery(ME);
-  const user = data?.me || [];
-  console.log(user)
-
-  if (loading) {
-    return <h2>Loading....</h2>
-  }
-
     return(
         <section>
             <header className="pad">
